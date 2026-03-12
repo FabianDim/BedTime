45a45f4d91b53479686520bfdf00619c73b69f23
Fabian <fdimento33@gmail.com>
2026-03-12 19:26:41 +1100

    Update Bedtime.py

--- Bedtime.py
+++ Bedtime.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import psutil
 import winrt.windows.ui.notifications as notifications
 
-apps = ["discord.exe", "chrome.exe", "steam.exe"]
+killapps = ["discord.exe", "chrome.exe", "steam.exe"]
 
 now = datetime.now()
 
@@ -11,5 +11,7 @@ if now.weekday() < 5 and now.hour == 23 and now.minute == 25:
     for proc in psutil.process_iter(['name']):
         if proc.info['name'] in apps:
             proc.kill()
+        if proc.info['name'] in TerminateApps:
+            proc.terminate()
 
     os.system("shutdown /s /t 0")
