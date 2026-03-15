55863ed39548f64cfce00d7eab41ff571738b5a3
Fabian <fdimento33@gmail.com>
2026-03-16 02:33:52 +1100

    Fix the venv add a test and notifications

--- Bedtime.py
+++ Bedtime.py
@@ -1,15 +1,45 @@
 import os
 from datetime import datetime
+import time
 import psutil
-import winrt.windows.ui.notifications as notifications
+from win11toast import toast
+kill_apps = ["Update.exe", "chrome.exe", "steam.exe"]
+terminate_apps = ["Ableton Live 12 Suite.exe"]
 
-apps = ["discord.exe", "chrome.exe", "steam.exe"]
-
-now = datetime.now()
-
-if now.weekday() < 5 and now.hour == 23 and now.minute == 25:
-    for proc in psutil.process_iter(['name']):
-        if proc.info['name'] in apps:
-            proc.kill()
-
+def shutdown():
     os.system("shutdown /s /t 0")
+
+def close_apps():
+    for proc in psutil.process_iter(['name']):
+        if proc.info['name'] in kill_apps:
+            proc.kill()
+        elif proc.info['name'] in terminate_apps:
+            proc.terminate()    
+
+
+def week_day_shutoff():
+    while True:
+        now = datetime.now()
+        time.sleep(360)
+        print("Checking time.")
+        if (now.hour == 00 and now.weekday() == 1) or (now.weekday() < 5 and now.hour == 23 and now.minute >= 25):
+            toast('5 minutes until forced shutdown', 
+                'Close your apps and save your work.')
+            time.sleep(300)
+            close_apps()
+            shutdown()
+
+def test_closure():
+    for proc in psutil.process_iter(['name']):
+        if proc.info['name'] in terminate_apps:
+            print(proc.info['name'])
+            proc.terminate()    
+        # toast('5 minutes until forced shutdown', 
+        # 'Close your apps and save your work. ')
+
+
+def main():
+    test_closure()
+
+if __name__ == "__main__":
+    main()
